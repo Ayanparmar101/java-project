fb5b1d91958efc386a1b4fcac1eeac5631d298a8
Ayan Parmar <140003817+Ayanparmar101@users.noreply.github.com>
2025-03-12 21:46:55 +0530

    Update Message.java

--- src/main/java/com/chatapp/model/Message.java
+++ src/main/java/com/chatapp/model/Message.java
@@ -22,3 +22,36 @@ public class Message {
         this.content = content;
         this.timestamp = LocalDateTime.now();
     }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getNickname() {
+        return nickname;
+    }
+    
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    
+    public String getContent() {
+        return content;
+    }
+    
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+    
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
