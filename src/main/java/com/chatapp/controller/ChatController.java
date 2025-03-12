e835e78c5f25dd9ac5d4fab0ff4986f1dbe0929b
Ayan Parmar <140003817+Ayanparmar101@users.noreply.github.com>
2025-03-12 21:47:45 +0530

    Create ChatController.java

---
+++ src/main/java/com/chatapp/controller/ChatController.java
@@ -0,0 +1,34 @@
+package com.chatapp.controller;
+
+import com.chatapp.model.Message;
+import com.chatapp.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class ChatController {
+    
+    @Autowired
+    private MessageRepository messageRepository;
+    
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Message> messages = messageRepository.findAll();
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+    
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam String nickname, @RequestParam String content) {
+        Message message = new Message(nickname, content);
+        messageRepository.save(message);
+        return "redirect:/";
+    }
